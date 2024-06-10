71a6efbe46cbe2a1e3f3817a5a28bc783f6dcbb8
Adarsh Pandey <adarsh435@icloud.com>
2024-06-10 12:56:17 +0530

    Update App.js

--- pizza-menu/src/App.js
+++ pizza-menu/src/App.js
@@ -1,9 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <h1>Hello React</h1>
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+      <Headers />
+    </div>
+  );
+}
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Margherita</h2>
+      <p>Tomato and mozarella</p>
+      <img src="pizzas/margherita.jpg" alt="Pizza Marghe"></img>
+    </div>
+  );
+  function Headers() {}
+}
+function Menu() {}
+function Headers() {
+  return (
+    <div>
+      <h1>Pizzaria Cafe Varanasi</h1>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <div>
+      <h5>We're now Open</h5>
+    </div>
   );
 }
 
